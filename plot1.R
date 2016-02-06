b55aa2c673de358ab186d14687e6c526e865da7a
jeturcotte <joshua.eric.turcotte@gmail.com>
2016-02-06 10:11:26 -0500

    now all there's to do is design each plot

--- plot1.R
+++ plot1.R
@@ -1,12 +1,11 @@
 # load the data
      src <- "./raw_data/household_power_consumption.txt"
      use_lines <- grep("[12]/2/2007",readLines(src))
-     pcd <- read.table(src, sep=";", header=T, skip=use_lines[1], nrows=nrow(use_lines))
+     pcd <- read.table(src, sep=";", header=T, skip=use_lines[1], nrows=length(use_lines))
      # under other circumstances, I would make a library to do this part
      # rather than duplicate it across four separate files... BUT...
 
-# plot the data
-     # make sure graphics device is selected
-     
-
 # save the file to PNG
+     png(filename="plot1.png")
+     # create the plot
+     dev.off()
